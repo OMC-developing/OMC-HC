70abde174456eaff50d13c88c978af45bd51733c
makzzz1986 <makzzz1986@gmail.com>
2018-07-10 14:09:37 +0000

    Primitive API for docker works now

--- rebuilt-run-hc.sh
+++ rebuilt-run-hc.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-cd /home/makzzz/OMC/OMC-HC/
-sudo docker build --no-cache -t omc-hc .
-sudo docker run -p 6880:6880 -v /var/run/docker.sock:/docker.sock -v /usr/local/OMC/:/usr/local/OMC -d omc-hc
+cd /home/makzzz/OMS/OMC-HC/
+sudo docker build -t omc-hc .
+
+# delete old build
+sudo docker rm omc-hc
+
+# deattach from tty
+sudo docker run --name omc-hc -p 6880:6880 -v /var/run/docker.sock:/var/run/docker.sock -v /usr/local/OMC/:/usr/local/OMC -d omc-hc
+
+# intercative mode
+#sudo docker run --name omc-hc -p 6880:6880 -v /var/run/docker.sock:/var/run/docker.sock -v /usr/local/OMC/:/usr/local/OMC -ti omc-hc
